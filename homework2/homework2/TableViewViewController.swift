618adabffbd1a1a47144f0f349b5c18c1d180ad5
itisioslab <itisioslab@Mac-mini-administrator-2.local>
2018-10-03 17:21:40 +0300

    dsd

---
+++ homework2/homework2/TableViewViewController.swift
@@ -0,0 +1,38 @@
+//
+//  TableViewViewController.swift
+//  homework2
+//
+//  Created by itisioslab on 03.10.2018.
+//  Copyright © 2018 FirstGroupCompany. All rights reserved.
+//
+
+import UIKit
+
+class TableViewViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tableView.estimatedRowHeight = 200
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
